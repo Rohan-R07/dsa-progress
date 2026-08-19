33f571d5c6d9accb790c65aab998a90253b5886e
Rohan R <05rohanr@gmail.com>
2026-08-19 23:04:55 +0530

    17 ms (9.11%) 08-19-2026 Easy  0026-remove-duplicates-from-sorted-array

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,15 +1,24 @@
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         seen = []
+#         duplicate = []
+#         for i in range(len(nums)):
+#             if nums[i] not in seen:
+#                 seen.append(nums[i])
+   
+        
+#         for i in range(len(seen)):
+#             nums.remove(seen[i])
+
+#         return len(nums)
+        
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        seen = []
-        duplicate = []
+        new_list = []
         for i in range(len(nums)):
-            if nums[i] in seen:
-                duplicate.append(nums[i])
-            else:
-                seen.append(nums[i])
+            if nums[i] not in new_list:
+                new_list.append(nums[i])
         
-        for i in range(len(duplicate)):
-            nums.remove(duplicate[i])
-  
-        return len(nums)
-        
+        for i in range(len(new_list)):
+            nums[i] = new_list[i]
+        return len(new_list)
