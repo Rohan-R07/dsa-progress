e5c196f3e921f9360ae62d60171df1cc46955947
Rohan R <05rohanr@gmail.com>
2026-08-20 23:23:51 +0530

    3 ms (2.81%) 08-20-2026 Easy  0035-search-insert-position

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        hashset = set(nums)
+        lens = len(nums)
+        for i in range(0,lens):
+            
+            if nums[i] == target:
+                return i
+            elif target not in hashset:
+                if i + 1 < lens:
+                    if target < nums[i+1] and target > nums[i]:   
+                        return i+1
+                elif target == nums[i] + 1 or target > nums[i] + 1:
+                    return lens
+
+            
+        return 0
