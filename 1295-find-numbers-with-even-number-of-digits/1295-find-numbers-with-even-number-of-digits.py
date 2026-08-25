ef841c0571403f307ab5edcedc750debc8e80db4
Rohan R <05rohanr@gmail.com>
2026-08-25 11:12:27 +0530

    3 ms (45.16%) 08-25-2026 Easy  1295-find-numbers-with-even-number-of-digits

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -1,13 +1,20 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        count = ""
+        # count = ""
+        # even = 0
+        # for i in range(len(nums)):
+        #     for k in range(len(str(nums[i]))):
+
+        #         count = str(k)
+            
+        #     if (int(count)+1)%2 == 0:
+        #         even+=1
+        # return even
+            
         even = 0
         for i in range(len(nums)):
-            for k in range(len(str(nums[i]))):
+            alpha = str(nums[i])
+            if len(alpha) % 2 == 0:
+                even += 1
 
-                count = str(k)
-            
-            if (int(count)+1)%2 == 0:
-                even+=1
-        return even
-            
+        return even    
