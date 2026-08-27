a3158a0debb6cc28c9b56a27bddfb069fc099ef2
Rohan R <05rohanr@gmail.com>
2026-08-27 20:42:39 +0530

    0 ms (100%) 08-27-2026 Easy  0412-fizz-buzz

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        newList = []
+
+        for i in range(1,n+1):
+            if (i % 3) ==0 and (i % 5) == 0:
+                newList.append("FizzBuzz")
+            elif (i % 3) == 0:
+                newList.append("Fizz")
+            elif (i % 5) == 0:
+                newList.append("Buzz")
+            else:
+                newList.append(str(i))
+
+        return newList
