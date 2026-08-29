df96a847a324b40fc4f16e6506b401c4364a9942
Rohan R <05rohanr@gmail.com>
2026-08-29 22:14:41 +0530

    47 ms (49.92%) 08-29-2026 Medium  0007-reverse-integer

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,24 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
+
+        absolute = abs(x)
         rev = 0
-        temp = abs(x)  # work with positive version        
-        while temp > 0:
-            lastDigit = temp%10
-            rev = rev * 10 + lastDigit
-            if rev <= -2**31:
+        while absolute > 0:
+            lastDigt = absolute % 10
+            rev = (rev*10) + lastDigt
+            absolute = absolute//10
+   
+        if x > 0:
+            if rev > 2147483647:
                 return 0
-            if rev >= 2**31-1:
-                return 0
-            temp = temp//10
-
-        if x < 0:
-            return -rev
+            
+            return rev 
         else:
-            return rev
-                
-
-
-        
-        
-
-        
+            if rev > 2147483647:
+                return 0
+            return -rev
