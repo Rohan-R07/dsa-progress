dbfc8aac8140dc1479b8509394e1abd190535954
Rohan R <05rohanr@gmail.com>
2026-08-30 11:42:38 +0530

    0 ms (100%) 08-30-2026 Easy  0744-find-smallest-letter-greater-than-target

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -1,10 +1,6 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
 
-        # for i in range(len(letters)):
-        #     if target < letters[i] :
-        #         return i
-       
         for i in letters:
             if target < i:
                 return i
