607b1222cafa4c3f73f034e010390b8fb76f66ae
Rohan R <05rohanr@gmail.com>
2026-08-31 22:10:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Max and Min Product from 2 Arrays/max-and-min-product-from-2-arrays.py
@@ -0,0 +1,10 @@
+
+
+class Solution:
+    
+    def minMaxProduct(self, arr1, arr2):
+        # code here
+        maxium = max(arr1)
+        minium = min(arr2)
+        
+        return maxium*minium
