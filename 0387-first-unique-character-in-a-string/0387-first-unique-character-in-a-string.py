9c07261fc507f4de8723dc3fbb8182555ec36aa1
Rohan R <05rohanr@gmail.com>
2026-08-31 21:24:49 +0530

    78 ms (31.61%) 08-31-2026 Easy  0387-first-unique-character-in-a-string

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+
+        frequency = {}
+        for i in range(len(s)):
+            if s[i] in frequency:
+                frequency[s[i]] += 1
+            else:
+                frequency[s[i]] = 1
+        newVar = ""
+        for key,value in frequency.items():
+            if value == 1:
+                newVar = key
+                break
+        for i in range(len(s)):
+            if s[i] == newVar:
+                return i
+        return -1
