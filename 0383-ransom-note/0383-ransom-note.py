609913e641829c418a384727242364985d58f961
Rohan R <05rohanr@gmail.com>
2026-09-03 19:29:13 +0530

    16 ms (67.47%) 09-03-2026 Easy  0383-ransom-note

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,14 +3,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
         frequencyRansom = Counter(ransomNote)
-
-        # for i in range(len(ransomNote)):
-        #     frequencyRansom[ransomNote[i]] = frequencyRansom.get(ransomNote[i],0)+1
-
         frequencyRansom1 = Counter(magazine)
-
-        # for i in range(len(magazine)):
-        #     frequencyRansom1[magazine[i]] = frequencyRansom1.get(magazine[i],0)+1
         
         for i in frequencyRansom:
             if i not in frequencyRansom1:
