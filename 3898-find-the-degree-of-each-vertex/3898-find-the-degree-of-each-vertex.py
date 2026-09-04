fa4d2fc22a9896a1ca628e1d6651931e9a582105
Rohan R <05rohanr@gmail.com>
2026-09-04 12:08:19 +0530

    4 ms (73.9%) 09-04-2026 Easy  3898-find-the-degree-of-each-vertex

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.py
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.py
@@ -2,9 +2,7 @@ class Solution:
     def findDegrees(self, matrix: list[list[int]]) -> list[int]:
         newList = []
         for i in range(len(matrix)):
-            sums = 0
-            for k in range(len(matrix[i])):
-                sums += matrix[i][k]
+            sums = sum(matrix[i])
                 
             newList.append(sums)
             
