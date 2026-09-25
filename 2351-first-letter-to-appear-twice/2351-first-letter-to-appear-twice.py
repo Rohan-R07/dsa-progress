5483b391c4db24d9f1451e7a046bf40e43f93b04
Rohan R <05rohanr@gmail.com>
2026-09-25 20:54:44 +0530

    0 ms (100%) 09-25-2026 Easy  2351-first-letter-to-appear-twice

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,22 +1,14 @@
-from collections import Counter
-
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
         
-        string = ""
-        freq = Counter(s)
-
         seen = []
         duplicate = ""
 
         for i in range(len(s)):
 
             if s[i] in seen :
-                duplicate = s[i]
-                break
+                return s[i]
             else:
                 seen.append(s[i])
 
-        for key,elements in freq.items():
-            if key == duplicate:
-                return key
+ 
